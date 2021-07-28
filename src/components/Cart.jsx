d7fc6e36b93f7a22be62cf725d51f0ebdaa3605e
nesbita <ariananesbit@gmail.com>
2021-07-28 12:41:05 -0600

    styled

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
+import Axios from 'axios'
+
+// import { setState } from 'react';
+
+// const apiURL = 'http://localhost:5000'
+// const { useState, useEffect } = React;
 
 const Cart = () => {
+    // const [products, setProducts] = useState([]);
+    // axios
+    //     .get(apiURL)
+    //         .then((response) => {
+    //             setState(response.message);
+    //         })
+    //     .catch((error) => {
+    //         console.log(error);
+    //     });
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setProducts([{ id: 0, name: '', description: ''}, { id: 1, content: '', name: '', description: ''}]);
+    //         console.log(products);
+    //     }, 4);
+    // }, []);
     return (
         <div class = 'cartContainer'>
             <h1>
                 Items in Cart
             </h1>
+            <ul>
+                <li>Sunflower Varieties  $25</li>
+            </ul>
+           {/* <div>{products.map(products => <div key={products.id}>{products.content}</div>)}</div> */}
         </div>
     )
 }
